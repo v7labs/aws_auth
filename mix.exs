cc0d4a8d8404969aae0100805eb64e1ce200534e
Rafał Radziszewski <rafal.radziszewski@gmail.com>
2021-09-06 10:59:51 +0200

    Added crypto as extra_application, since package depends on it

--- mix.exs
+++ mix.exs
@@ -2,25 +2,26 @@ defmodule AWSAuth.Mixfile do
   use Mix.Project
 
   def project do
-    [app: :aws_auth,
-     version: "0.7.2",
-     elixir: "~> 1.3",
-     description: description(),
-     package: package(),
-     deps: deps(),
-     test_coverage: [tool: ExCoveralls],
-     preferred_cli_env: [coveralls: :test]
+    [
+      app: :aws_auth,
+      version: "0.7.2",
+      elixir: "~> 1.12",
+      description: description(),
+      package: package(),
+      deps: deps(),
+      test_coverage: [tool: ExCoveralls],
+      preferred_cli_env: [coveralls: :test]
     ]
   end
 
   def application do
-    [applications: [:logger]]
+    [applications: [:logger], extra_applications: [:crypto]]
   end
 
   defp deps do
     [
-      {:earmark, "~> 1.2.3", only: :dev },
-      {:ex_doc, "~> 0.16", only: :dev },
+      {:earmark, "~> 1.2.3", only: :dev},
+      {:ex_doc, "~> 0.16", only: :dev},
       {:excoveralls, "~> 0.4", only: :test},
       {:credo, "~> 0.8.6", only: [:dev, :test]}
     ]
@@ -34,10 +35,10 @@ defmodule AWSAuth.Mixfile do
 
   defp package do
     [
-     files: ["lib", "mix.exs", "README*"],
-     maintainers: ["Bryan Joseph"],
-     licenses: ["Apache 2.0"],
-     links: %{"GitHub" => "https://github.com/bryanjos/aws_auth"}
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Bryan Joseph"],
+      licenses: ["Apache 2.0"],
+      links: %{"GitHub" => "https://github.com/bryanjos/aws_auth"}
     ]
   end
 end
